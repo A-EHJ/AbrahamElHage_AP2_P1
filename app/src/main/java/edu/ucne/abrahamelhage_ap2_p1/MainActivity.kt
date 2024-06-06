0dc5bd2298427e36d414dee01f9c3dfac2e09994
A-EHJ <Abrahamelhage2003@gmail.com>
2024-06-05 20:47:29 -0400

    haciendo el build de la bd y finalizando

--- app/src/main/java/edu/ucne/abrahamelhage_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/abrahamelhage_ap2_p1/MainActivity.kt
@@ -4,42 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import edu.ucne.abrahamelhage_ap2_p1.data.local.database.ServicioDb
+import edu.ucne.abrahamelhage_ap2_p1.data.repository.ServicioRepository
 import edu.ucne.abrahamelhage_ap2_p1.presentation.navigation.Parcial1NavHost
 import edu.ucne.abrahamelhage_ap2_p1.ui.theme.AbrahamElHage_AP2_P1Theme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var servicioDb: ServicioDb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        servicioDb = Room.databaseBuilder(
+            this,
+            ServicioDb::class.java,
+            "Servicio.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+        val servicioRepository = ServicioRepository(servicioDb.servicioDao())
+
         enableEdgeToEdge()
         setContent {
             AbrahamElHage_AP2_P1Theme {
-                    val navController = rememberNavController()
-                    Parcial1NavHost(navHostController = navController)
+                val navController = rememberNavController()
+                Parcial1NavHost(navHostController = navController, repository = servicioRepository)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AbrahamElHage_AP2_P1Theme {
-        Greeting("Android")
-    }
-}
